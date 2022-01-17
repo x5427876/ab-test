f0c17d372c30c2d875995c3cc68ab1c7f915d4fd
Gavin Gong <gavingong@110N000026.local>
2022-01-17 18:19:52 +0800

    add button

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" className="button">這是測試A</button>
+        <button type="button" className="button">這是測試B</button>
       </header>
     </div>
   );
